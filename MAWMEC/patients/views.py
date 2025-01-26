fb77d0e25b07e23e61fef0afe8071de8cd2c9c5b
Nehemiah Kemayah <ingeniouskemayah@gmail.com>
2025-01-26 20:14:46 +0000

    login page html and css done. working on functionalities

--- MAWMEC/patients/views.py
+++ MAWMEC/patients/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def home_view(request):
+    user = request.user
+    return render(request, 'patients/home.html', {'user':user})
