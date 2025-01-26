527959f8cfa5840ba4dfc1092d0b2681da224370
Nehemiah Kemayah <ingeniouskemayah@gmail.com>
2025-01-26 22:09:15 +0000

    correction of login routes

--- MAWMEC/core/views.py
+++ MAWMEC/core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/index.html')
+
+def login(request):
+    return render(request, 'authentication/login.html')
